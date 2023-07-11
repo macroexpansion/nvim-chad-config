534151104d7f4cc5fcb51afa2c75c71a58430801
blueyellowpink <andrew@trasua.dev>
2023-07-11 23:55:57 +0700

    remove mini.bufremove

--- configs/mini.lua
+++ configs/mini.lua
@@ -24,7 +24,6 @@ require 'mini.move'.setup({
 require 'mini.comment'.setup()
 require 'mini.basics'.setup()
 require 'mini.indentscope'.setup()
-require 'mini.bufremove'.setup()
 require 'mini.trailspace'.setup()
 -- require('mini.tabline').setup()
 
