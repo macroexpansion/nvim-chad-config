f1bc72c5f9fc86b1bb65ce0c986f6625ae208ce0
blueyellowpink <andrew@trasua.dev>
2023-07-24 02:43:58 +0700

    update telescope

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -6,15 +6,24 @@ local M = {
             n = {
                 ["<C-j>"] = actions.preview_scrolling_down,
                 ["<C-k>"] = actions.preview_scrolling_up,
-                -- ["<C-h>"] = actions.preview_scrolling_left,
-                -- ["<C-l>"] = actions.preview_scrolling_right,
+                ["<C-h>"] = actions.preview_scrolling_left,
+                ["<C-l>"] = actions.preview_scrolling_right,
             },
             i = {
                 ["<C-j>"] = actions.preview_scrolling_down,
                 ["<C-k>"] = actions.preview_scrolling_up,
-                -- ["<C-h>"] = actions.preview_scrolling_left,
-                -- ["<C-l>"] = actions.preview_scrolling_right,
+                ["<C-h>"] = actions.preview_scrolling_left,
+                ["<C-l>"] = actions.preview_scrolling_right,
             },
+        },
+        layout_config = {
+            horizontal = {
+                preview_width = 0.5,
+                results_width = 0.5,
+            },
+            width = 0.95,
+            height = 0.93,
+            preview_cutoff = 120,
         },
     },
 }
