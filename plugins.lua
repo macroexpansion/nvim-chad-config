c533ddeacc76d5fba5533da33ffca3db6dbb1576
blueyellowpink <andrew@trasua.dev>
2023-07-24 11:48:49 +0700

    update flash

--- plugins.lua
+++ plugins.lua
@@ -51,6 +51,28 @@ local plugins = {
                 end,
                 desc = 'Flash Jump',
             },
+            {
+                '<leader>s',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require 'flash'.jump({
+                        pattern = '.', -- initialize pattern with any char
+                        search = {
+                            mode = function(pattern)
+                                -- remove leading dot
+                                if pattern:sub(1, 1) == '.' then
+                                    pattern = pattern:sub(2)
+                                end
+                                -- return word pattern and proper skip pattern
+                                return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                            end,
+                        },
+                        -- select the range
+                        jump = { pos = 'range' },
+})
+                end,
+                desc = 'Flash Jump Any Word',
+            },
         },
     },
     -- {
