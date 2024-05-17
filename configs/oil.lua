bc7eccf0937d7710ab2bce3b3a91f10f04669c38
macroexpansion <trasuadev@gmail.com>
2024-05-17 10:18:21 +0700

    oil: default show hidden files

--- configs/oil.lua
+++ configs/oil.lua
@@ -1,3 +1,8 @@
-require("oil").setup()
+require("oil").setup {
+  view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = true,
+  },
+}
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })
