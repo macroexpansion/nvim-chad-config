82488b2a40d5257186bba6df4f37e7973c9208e8
macroexpansion <macroexpansion@gmail.com>
2025-06-13 13:17:16 +0700

    add git-conflict, diffview

--- plugins/init.lua
+++ plugins/init.lua
@@ -393,4 +393,57 @@ return {
     "augmentcode/augment.vim",
     event = "InsertEnter",
   },
+
+  {
+    "akinsho/git-conflict.nvim",
+    event = "VeryLazy",
+    version = "v2.1.0",
+    opts = {
+      default_mappings = false, -- disable buffer local mapping created by this plugin
+      default_commands = true, -- disable commands created by this plugin
+      disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+      list_opener = "copen", -- command or function to open the conflicts list
+      highlights = { -- They must have background color, otherwise the default color will be used
+        incoming = "GitConflictDiffAdd",
+        current = "GitConflictDiffText",
+      },
+    },
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "git-conflict")
+      require("git-conflict").setup(opts)
+    end,
+  },
+
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   lazy = false,
+  --   version = "v4.9.1",
+  --   dependencies = "nvim-tree/nvim-web-devicons",
+  --   opts = {
+  --     options = {
+  --       offsets = {
+  --         {
+  --           filetype = "NvimTree",
+  --           text = "File Explorer",
+  --           highlight = "Directory",
+  --           text_align = "left",
+  --         },
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     dofile(vim.g.base46_cache .. "bufferline")
+  --     require("bufferline").setup(opts)
+  --   end,
+  -- },
+
+  {
+    "sindrets/diffview.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "diffview")
+      require("diffview").setup(opts)
+    end,
+  },
 }
