dd403ffaea08d66087cbeeeacb7257d6bc1d9a49
macroexpansion <macroexpansion@gmail.com>
2025-07-12 00:24:32 +0700

    add typescript-tools.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "pmizio/typescript-tools.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("typescript-tools").setup {}
+    end,
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     cmd = "Telescope",
