b1329cc0a028d6f73677f0a52b52291590f1f7ba
macroexpansion <macroexpansion@gmail.com>
2026-01-12 13:03:39 +0700

    remove comments

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,24 +4,6 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- EXAMPLE
--- local servers = { "html", "cssls" }
--- lsps with default config
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---   }
--- end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
 lspconfig.rust_analyzer.setup {
   on_attach = function(client, bufnr)
     local util = require "configs.lsputil"
